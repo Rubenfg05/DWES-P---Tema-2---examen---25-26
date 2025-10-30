e83a3b654fec9fb9b8dec32510c38043db29cafe
Rubenfg05 <you@example.com>
2025-10-30 12:09:48 +0100

    realizamos bebida.php

--- model/Bebida.php
+++ model/Bebida.php
@@ -1,2 +1,12 @@
 <?php
 // Clase Bebida que hereda de Artículo
+class Bebida extends Articulo {
+    public $tamano;
+    public $temperatura; 
+
+    public function __construct($nombre, $precio, $disponibilidad, $categoria, $tamano, $temperatura) {
+        parent::__construct($nombre, $precio, $disponibilidad, $categoria);
+        $this->tamano = $tamano;
+        $this->temperatura = $temperatura;
+    }
+}
